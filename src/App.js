b8944719da2cdb582c6399a1a72c539983258fc6
rafaelavb <rafaelvanberkel@hotmail.com>
2022-08-03 10:41:18 +1200

    added shuffleCards function and React useState

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import './App.css'
 
 //images are constant and never need to change so dont need to be stored in any component state. By creating the const cardImages outside of the cards array, it wont be recreated everytime the component is re-evaluated
@@ -12,10 +13,36 @@ const cardImages = [
 ]
 
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  // Shuffle Cards function //
+
+  const shuffleCards = () => {
+    //Everytime a new game is started by clicking the 'new game' button, we are going to call this function shuffleCards which will shuffle the cards. It's going to set the cards (setCards) to be the shuffledCards constant and then it will also reset the turns back to 0 everytime the button is clicked.
+
+    const shuffledCards = [...cardImages, ...cardImages]
+      //using the spread operator two times means we get 2 sets of the array 'cardImages'.
+
+      .sort(() => Math.random() - 0.5)
+      // The sort method fires a function for each item or pair of items in the array
+
+      //Math.random - If we return a number less than 0, the order of the two items stays the same, if we return a number greater than 0, the order of the two items it is comparing is mixed up. This gives us a shuffled array.
+
+      .map((card) => ({ ...card, id: Math.random() }))
+    //The map method fires a function for each item in the new shuffled array. For each item, we add a random id property. Each item is represented by the 'card' object in the .map function.
+
+    setCards(shuffledCards)
+    //When the shuffleCards function is called, it will generate the shuffledCards and then update the cards state to be shuffledCards
+    setTurns(0)
+  }
+
+  console.log(cards, turns)
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   )
 }
